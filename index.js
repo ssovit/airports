171310e67f010a56c3043c0a8ddc0a37a12f8a5e
Sovit Tamrakar <sovit.tamrakar@gmail.com>
2023-03-23 13:37:08 +0545

    update

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const fs = require("fs");
 try {
   let rawData = JSON.parse(fs.readFileSync("./airports.json"));
-  fs.writeFileSync("./airports.json", JSON.stringify(rawData.response));
+  if (rawData.response && rawData.response.length > 0) {
+    fs.writeFileSync("./airports.json", JSON.stringify(rawData.response));
+  }
 } catch (e) {
   console.log(e);
 }
