50569e0861473ced5eafdd904fbca3da7b796079
Sovit Tamrakar <sovit.tamrakar@gmail.com>
2023-03-26 12:56:53 +0545

    update

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const fs = require("fs");
+const lookup = require("country-code-lookup");
+
 try {
   let rawData = JSON.parse(fs.readFileSync("./airports.json"));
   if (rawData.response && rawData.response.length > 0) {
-    fs.writeFileSync("./airports.json", JSON.stringify(rawData.response));
+    const airports = rawData.response.map((airport) => {
+      const country = lookup.byIso(airport.country_code);
+      return {
+        iata: airport.iata_code || '',
+        icao: airport.icao_code || '',
+        name: airport.name || '',
+        country: country.country || '',
+      };
+    });
+    fs.writeFileSync("./airports.json", JSON.stringify(airports));
   }
 } catch (e) {
   console.log(e);
