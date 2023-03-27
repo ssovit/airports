958e9a4ae20fc59dc69c06cfd1fe6be9990f1397
Sovit Tamrakar <sovit.tamrakar@gmail.com>
2023-03-27 09:59:10 +0545

    update

--- index.js
+++ index.js
@@ -1,21 +1,44 @@
 const fs = require("fs");
 const lookup = require("country-code-lookup");
-
+const needle = require("needle");
+const csvParser = require("csv-parser");
 try {
-  let rawData = JSON.parse(fs.readFileSync("./airports.json"));
-  if (rawData.response && rawData.response.length > 0) {
-    const airports = rawData.response.map((airport) => {
-      const country = lookup.byIso(airport.country_code);
-      return {
-        iata: airport.iata_code || '',
-        icao: airport.icao_code || '',
-        name: airport.name || '',
-        country: country.country || '',
-        city: airport.city || '',
-      };
+  let result = [];
+  needle
+    .get("https://davidmegginson.github.io/ourairports-data/airports.csv")
+    .pipe(csvParser())
+    .on(
+      "data",
+      ({
+        iata_code,
+        iso_country,
+        type,
+        gps_code,
+        name,
+        keywords,
+        municipality,
+      }) => {
+        if (iata_code.length > 0) {
+          if (
+            ["small_airport", "medium_airport", "large_airport"].includes(type)
+          ) {
+            const { country } = lookup.byIso(iso_country);
+            result.push({
+              name: name,
+              alt_name: keywords,
+              country,
+              country_code: iso_country,
+              city: municipality,
+              iata: iata_code,
+              icao: gps_code,
+            });
+          }
+        }
+      }
+    )
+    .on("done", (err) => {
+      fs.writeFileSync("./airports.json", JSON.stringify(result, "", 2));
     });
-    fs.writeFileSync("./airports.json", JSON.stringify(airports));
-  }
 } catch (e) {
   console.log(e);
 }
---
+++ readme.md
@@ -0,0 +1,6 @@
+## Airports data in JSON
+
+use `airports.json` file
+
+Special thanks to https://github.com/davidmegginson/ourairports-data for proving airports csv file which is used on this project.
+
